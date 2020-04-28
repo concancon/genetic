b57b460e390e4cce4f843f206caf295e87e471b1
Camilo Ocampo <camiloocampo@Camilos-MacBook-Pro.local>
2020-04-28 09:38:56 +0200

    Finish porting and add output

--- min.genetic.cpp
+++ min.genetic.cpp
@@ -12,30 +12,29 @@
 #include <cstdlib>
 
 using namespace std;
-class Population{
-public:
-    vector<int> targetParams;
-    Population(){
-        
-        this->targetParams= {1, 2, 3};
-    }
-  
+
+static double map(double value, int start1, double stop1,
+                 int start2, int stop2) {
+    double outgoing =
+    start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
     
-    
-};
-
-
-
+    return outgoing;
+}
 
 class DNA{
     
 public:
+    
+    float fitness= 0;
+    std::vector<int> genes;
+    int numberOfGenes;
     //constructor for DNA class
-    DNA(int numberOfGenes){
-        this->numberOfGenes= numberOfGenes; // change this to target.size()
+    DNA(){
+       
+        this->numberOfGenes= 3; // TODO: change this to target.size()
         int randomGene= 0;
         for(int i = 0; i< numberOfGenes; i++){
-            randomGene= rand() % 127;
+            randomGene= rand() % 128;
             genes.push_back(randomGene);
         }
         
@@ -48,6 +47,7 @@ public:
         // elem 2: vca loudness
         int score = 0;
         for(int i = 0; i< genes.size(); i++){
+            
             if(genes[i] == target[i]){
                 score++;
             }
@@ -60,7 +60,7 @@ public:
     
     DNA crossover(DNA partner){
         
-        DNA child(this->numberOfGenes);
+        DNA child;
         int midpoint = rand() % this->numberOfGenes;
         for(int i = 0; i< this->numberOfGenes; i++){
             if(i > midpoint){
@@ -79,7 +79,7 @@ public:
         for(int i= 0; i< this->numberOfGenes; i++){
             double r = ((double) rand() / (RAND_MAX));
             if(r < mutationRate){
-                this->genes[i] = rand() % 127;
+                this->genes[i] = rand() % 128;
             }
         }
         
@@ -87,18 +87,148 @@ public:
     
     void displayGenes(){
         for(auto it: genes){
-            cout<< it <<std::endl;
+            cout<< it << endl;
         }
     }
 
-   
-private:
+
     
-    std::vector<int> genes;
-    float fitness= 0;
-    int numberOfGenes;
+    
+   
     
 };
+
+
+
+class Population{
+public:
+    
+    vector<DNA> population;
+    vector<DNA> matingPool;
+    vector<int> targetParams;
+    double mutationRate;
+    int maxPopulation;
+    
+    int generations;              // Number of generations
+    int finished;
+    int perfectScore;
+    
+    //population can also serve as an interface
+    //instead of using a constructor with 3 params
+    //we call the default constructor and then
+    //assign the attributes
+    //target paramters, mutation rate, and max population number
+    Population(){
+        cout << "default constructor called" <<endl;
+        targetParams= {12, 127, 38}; // find a better way to init these
+        mutationRate= 0;
+        maxPopulation= 1000;
+        finished = false;
+        //DNA population[maxPopulation];
+        
+        for(int i = 0; i< maxPopulation; i++){
+            DNA dna;
+            population.push_back(dna);
+            
+        }
+        cout << "population size at the end of constructor" << population.size()<<endl;
+            calcFitness();
+    }
+    
+    void calcFitness(){
+    
+          
+        for (int i = 0; i < population.size(); i++) {
+       
+            population[i].fitnessFunction(targetParams);
+           
+            
+       }
+    }
+    // Compute average fitness for the population
+     double getAverageFitness() {
+       double total = 0;
+       for (int i = 0; i < population.size(); i++) {
+           total += population[i].fitness;
+       }
+         cout << "total is " << total<<endl;
+       return total / (population.size());
+     }
+    
+    // Generate a mating pool
+    void naturalSelection() {
+      // Clear the vector
+        matingPool.clear();
+
+        double maxFitness = 0;
+        for (int i = 0; i < population.size(); i++) {
+        if (population[i].fitness > maxFitness) {
+          maxFitness = population[i].fitness;
+        }
+      }
+    
+    // Based on fitness, each member will get added to the mating pool a certain number of times
+      // a higher fitness = more entries to mating pool = more likely to be picked as a parent
+      // a lower fitness = fewer entries to mating pool = less likely to be picked as a parent
+      for (int i = 0; i < population.size(); i++) {
+        
+        float fitness = map(population[i].fitness,0, maxFitness,0,1);
+        int n = int(fitness * 100);  // Arbitrary multiplier, we can also use monte carlo method
+        for (int j = 0; j < n; j++) {              // and pick two random numbers
+          matingPool.push_back(population[i]);
+        }
+      }
+    }
+    
+     // Compute the current "most fit" member of the population
+        vector<int> getBest(){
+      
+         float maxFitness = 0.0;
+         int index= 0;
+         vector<int> best;
+         for (int i = 0; i < population.size(); i++) {
+         
+             if (population[i].fitness > maxFitness) {
+           maxFitness = population[i].fitness;
+           index = i;
+           }
+           
+         }
+         if(maxFitness == perfectScore){
+          finished = true;
+         }
+        return population[index].genes;
+
+    }
+    // Create a new generation
+    void generate() {
+      // Refill the population with children from the mating pool
+      for (int i = 0; i < population.size(); i++) {
+        int a = int(rand() % matingPool.size());
+        int b = int(rand() % matingPool.size());
+        DNA partnerA = matingPool[a];
+        DNA partnerB = matingPool[b];
+        DNA child = partnerA.crossover(partnerB);
+        child.mutate(mutationRate);
+        population[i] = child;
+      }
+      generations++;
+    }
+        
+    
+    bool terminate() {
+       return finished;
+     }
+
+     int getGenerations() {
+       return generations;
+     }
+    
+};
+
+
+
+
 
 using namespace c74::min;
 
@@ -106,54 +236,83 @@ using namespace c74::min;
 class genetic : public object<genetic> {
 public:
     
-  genetic(const atoms& args = {}) {
-      cout << "constructor called!" << c74::min::endl;
-      Population population;
-    
-  }
-
-    
 	MIN_DESCRIPTION {"apply genetic algorithm to n params"};
 	MIN_TAGS {"time"};
-	MIN_AUTHOR {"Cycling '74"};
+	MIN_AUTHOR {"Cycling 74"};
 	MIN_RELATED {"min.beat.pattern, link.beat, metro, tempo, drunk"};
 
    
-    
+  
 	inlet<>  input {this, "(toggle) on/off"};
-	outlet<> bang_out {this, "(bang) triggers at randomized interval"};
-	outlet<> interval_out {this, "(float) the interval for the current bang"};
-
-
+	outlet<> output {this, "(list) result of evolution"};
+	
+    Population population;
+    
+    
     attribute<vector<double>> target {this, "target", {1.0, 1.0 ,1.0},
 		setter { MIN_FUNCTION {
-			//vector<c74::min::atom> values = args;
-            //add this to the population class
+			
+            population = Population(); //reinitialize population to account for changing target
+            
             population.targetParams.clear();
             for(auto it: args){
-                population.targetParams.push_back((int)it);
-            }
+                        population.targetParams.push_back((int)it);
+                           }
+        
+           
             return {args};
         }}};
 
-
     message<> bang {
     this, "bang", "test the functionality of DNA class.", MIN_FUNCTION {
+        
+        vector<int> currentBest;
+        cout << "target params"<<c74::min::endl;
 
-
-        //test.displayGenes();
-        cout << "target is :" << c74::min::endl;
-        for(auto it: population.targetParams){
-
-            cout << it << c74::min::endl;
+        for(auto it : population.targetParams){
+               cout << it <<" " ;
+               
+           }
+        cout <<c74::min::endl;
+        
+       
+        if(!population.terminate()){
+        // Generate mating pool
+        population.naturalSelection();
+        //Create next generation
+        population.generate();
+        // Calculate fitness
+        population.calcFitness();
+        currentBest= population.getBest();
+       
         }
+        cout << "current best: "<<c74::min::endl;
+        
+        atoms result;
+        for(auto it : currentBest){
+            cout << it <<" " ;
+            result.push_back(it);
+            
+        }
+        cout <<c74::min::endl;
+        cout << "average: " <<population.getAverageFitness()<< c74::min::endl;
 
+        //cast current best to atoms
+        
+        output.send(result);
+        //        cout << "population itself" <<c74::min::endl;
+//
+//        for(int i = 0; i < population.population.size(); i++){
+//            for(auto it : population.population[i].genes){
+//        cout << it  << " " ;
+//            }
+//        cout <<c74::min::endl;
+//        }
+       
         return {};
     }};
-
-
-    private:
-        Population population;
+        
+       
 
 
 };
